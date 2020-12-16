eae190ad75161ff405f147a90092549062a01218
CI-Gary.Lin <gary.lin@cloud-interactive.com>
2020-12-16 14:22:07 +0800

    superUser

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Langauge
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
+admin.site.register(Langauge)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
--- catalog/models.py
+++ catalog/models.py
@@ -9,6 +9,8 @@ class Genre(models.Model):
         """String for representing the Model object."""
         return self.name
 
+from django.urls import reverse
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -22,6 +24,7 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    langauge = models.ForeignKey('Langauge', on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
         """String for representing the Model object."""
@@ -30,6 +33,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
 
@@ -79,3 +88,10 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+class Langauge(models.Model):
+    langauge = models.CharField(max_length=200)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.langauge
